ee4e7b35fcf22384c76cc4f80399ca89f341d70c
Pete5145 <peterosakpolor661@gmail.com>
2022-02-14 15:18:07 -0800

    added the input event

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 let tweetBox = document.querySelector("#txtBox");
 let displayBox = document.querySelector("#displayBox");
 
-tweetBox.addEventListener("keypress", () => {
+tweetBox.addEventListener("input", () => {
     let numOfWords = tweetBox.value.length;
     displayBox.innerHTML = `You have used ${numOfWords} characters and you have ${280 - numOfWords} left!`
     displayBox.style.color = "Blue";
 
-    if(numOfWords >= 280) {
+    if(numOfWords > 280) {
         displayBox.innerHTML = "You have exceeded the numbers of characters!";
         displayBox.style.color = "red";
         tweetBox.addEventListener("keydown", e => {
                 e.preventDefault();
                 return false;
         });
+        alert("You have exceeded the numbers of characters!");
     }  
     });
