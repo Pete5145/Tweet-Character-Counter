7e7117cfeb9a6385353b322826a35db3152686fe
Pete5145 <peterosakpolor661@gmail.com>
2022-02-14 14:01:26 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Select all the elements of interest
+let tweetBox = document.querySelector("#txtBox");
+let displayBox = document.querySelector("#displayBox");
+
+tweetBox.addEventListener("keypress", () => {
+    let numOfWords = tweetBox.value.length;
+    displayBox.innerHTML = `You have used ${numOfWords} characters and you have ${280 - numOfWords} left!`
+    displayBox.style.color = "Blue";
+
+    if(numOfWords >= 280) {
+        displayBox.innerHTML = "You have exceeded the numbers of characters!";
+        displayBox.style.color = "red";
+        tweetBox.addEventListener("keydown", e => {
+                e.preventDefault();
+                return false;
+        });
+    }  
+    });
